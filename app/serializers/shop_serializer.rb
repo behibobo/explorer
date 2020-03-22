e295a36350788adad0a5e0c364f53e03de182c8b
behzad <behzadfareed@gmail.com>
2020-03-22 14:06:31 -0400

    add state to shop serializer

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,8 +1,20 @@
 class ShopSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :image, :phone, :city, :items, :item_count
+  attributes :id, :name, :address, :image, :phone, :city, :city_id, :items, :item_count, :state, :state_id
   
   def city
     object.city.name
+  end
+
+  def city_id
+    object.city.id
+  end
+
+  def state_id
+    object.city.state.id
+  end
+
+  def state
+    object.city.state.name
   end
 
   def item_count
