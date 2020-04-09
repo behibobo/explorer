8c6339e192075c5b4907763e826860e20230f3ea
behzad <behzadfareed@gmail.com>
2020-04-09 16:51:08 +0430

    items

--- app/controllers/app/user_controller.rb
+++ app/controllers/app/user_controller.rb
@@ -7,25 +7,16 @@ class App::UserController < AppController
             last_name: current_user.last_name,
             credit: current_user.credit,
             gender: current_user.gender,
-            email: current_user.email
+            email: current_user.email,
+            mobile: current_user.mobile,
+            dob: current_user.dob&.to_date&.to_pdate&.to_s
         }
 
-
-        codes = ItemCode.where(user: current_user).order(scan_date: :desc)
-        items = []
-        codes.each do |code|
-            item = {
-                scan_date: code.scan_date.to_date.to_pdate.to_s,
-                item_name: code.item.name,
-                item_brand: code.item.brand,
-                item_image: code.item.image_url,
-                has_gift: !code.gift.nil?,
-                gift_value: (!code.gift.nil?)? code.gift.value : "",
-            }
-        items.push item
-        end
         data['user'] = user
-        data['items'] = items
+        data['total_items'] = Item.count
+        data['total_gifts'] = ItemCode.where.not(gift: nil).count
+        data['min_gift'] = Gift.order(:value).first.value
+        data['max_gift'] = Gift.order(:value).last.value
         render json: data.to_json
     end
      
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,7 +3,7 @@ class JsonWebToken
   # our secret key to encode our jwt
 
     class << self
-      def encode(payload, exp = 10.hours.from_now)
+      def encode(payload, exp = 10.years.from_now)
         # set token expiration time
         payload[:exp] = exp.to_i
 
