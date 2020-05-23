1ba15173175685e155d7bbbc2aded9bd0d7a1325
behzad <behzadfareed@gmail.com>
2020-05-23 21:10:56 +0430

    sss

--- app/serializers/treasure_serializer.rb
+++ app/serializers/treasure_serializer.rb
@@ -1,5 +1,5 @@
 class TreasureSerializer < ActiveModel::Serializer
-  attributes :id, :value, :valid_to, :lat, :lng, :required_credit, :state_id
+  attributes :id, :value, :valid_to, :lat, :lng, :required_credit, :state_id, :vali_to_shamsi
 
   def vali_to_shamsi
     object.valid_to&.to_date&.to_pdate.to_s
