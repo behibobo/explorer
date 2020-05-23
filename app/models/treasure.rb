69bb0a5fbf1a9e920e6a04eaf00d7d1b4640a7db
behzad <behzadfareed@gmail.com>
2020-05-23 21:24:30 +0430

    state

--- app/models/treasure.rb
+++ app/models/treasure.rb
@@ -1,5 +1,7 @@
 class Treasure < ApplicationRecord
     before_create :create_unique_identifier
+
+    belongs_to :state
     def create_unique_identifier
         loop do
           self.uuid = SecureRandom.uuid()
--- app/serializers/treasure_serializer.rb
+++ app/serializers/treasure_serializer.rb
@@ -1,7 +1,12 @@
 class TreasureSerializer < ActiveModel::Serializer
-  attributes :id, :value, :valid_to, :lat, :lng, :required_credit, :state_id, :valid_to_shamsi
+  attributes :id, :value, :valid_to, :lat, :lng, :required_credit, :state_id, :valid_to_shamsi, :state
 
   def valid_to_shamsi
     object.valid_to&.to_date&.to_pdate&.to_s&.gsub("-", "/")
   end
+
+  def state
+    object.state.name
+  end
+
 end
